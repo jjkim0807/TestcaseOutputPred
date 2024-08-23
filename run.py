4aa1da4935dce93568e92ed930ade5ce63c1846e
Jaejin Kim <jaejin.kim@snu.ac.kr>
2024-08-23 08:38:29 +0000

    add where query

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import fire
-
 from expand_langchain.evaluator import Evaluator
 from expand_langchain.generator import Generator
+from expand_langchain.loader import Loader
 
 if __name__ == "__main__":
     fire.Fire(
         {
             "generator": Generator,
             "evaluator": Evaluator,
+            "loader": Loader,
         }
     )

69f104a236e1d4d7ec13869e450560410db001df
Jaejin Kim <jaejin.kim@snu.ac.kr>
2024-11-29 07:24:29 +0000

    add custom chains (contd)

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from expand_langchain.evaluator import Evaluator
 from expand_langchain.generator import Generator
 from expand_langchain.loader import Loader
 
+from src.custom_chains import *
+
 if __name__ == "__main__":
     fire.Fire(
         {
